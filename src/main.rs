cb1d29a502d73d74b8ca098872a0fc387f9852d8
James Edwards <jedwards@pop-os.localdomain>
2020-05-10 19:18:52 +0100

    2nd commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+
+#[async_std::main]
+async fn main() -> Result<(), Error> {
+    dotenv::dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").unwrap();
+    let mut app = tide::new();
+    app.at("/").get(|_| async move { Ok("Hello, world!") });
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    #[error(transparent)]
+    DbError(#[from] sqlx::Error),
+
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
 }
