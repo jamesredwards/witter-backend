ab660f48009ad6008f59df4f503c6db0a35f611a
James Edwards <jedwards@pop-os.localdomain>
2020-05-10 21:22:50 +0100

    2nd commit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use dotenv;
+use sqlx::{query, PgPool, Pool};
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
     dotenv::dotenv().ok();
-    let db_url = std::env::var("DATABASE_URL").unwrap();
+    pretty_env_logger::init();
+    let db_url = std::env::var("DATABASE_URL")?;
+    let db_pool: PgPool = Pool::new(&db_url).await?;
+
+    let rows = query!("select 1 as one").fetch_one(&db_pool).await?;
+
     let mut app = tide::new();
     app.at("/").get(|_| async move { Ok("Hello, world!") });
     app.listen("127.0.0.1:8080").await?;
@@ -17,4 +23,7 @@ enum Error {
 
     #[error(transparent)]
     IoError(#[from] std::io::Error),
+
+    #[error(transparent)]
+    VarError(#[from] std::env::VarError),
 }
