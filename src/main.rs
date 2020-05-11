760efcd17e5adbb58627c6557c14db0e850b972a
jedwards <jamesredwards@me.com>
2020-05-11 21:04:35 +0100

    compiling with postgres login

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use dotenv;
 use sqlx::{query, PgPool, Pool};
+use tide::Server;
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
@@ -9,11 +10,16 @@ async fn main() -> Result<(), Error> {
     let db_pool: PgPool = Pool::new(&db_url).await?;
 
     let rows = query!("select 1 as one").fetch_one(&db_pool).await?;
+    dbg!(rows);
 
-    let mut app = tide::new();
+    let mut app = Server::with_state(State { db_pool });
     app.at("/").get(|_| async move { Ok("Hello, world!") });
     app.listen("127.0.0.1:8080").await?;
     Ok(())
+}
+
+struct State {
+    db_pool: PgPool,
 }
 
 #[derive(thiserror::Error, Debug)]
